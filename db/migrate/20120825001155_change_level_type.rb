3a134785488cd6c6a776a3c1647cd0ae435e6a86
Max Olson <maxprogram@gmail.com>
2012-08-25 15:20:21 -0600

    Removed old migrations

--- db/migrate/20120825001155_change_level_type.rb
+++
@@ -1,8 +0,0 @@
-class ChangeLevelType < ActiveRecord::Migration
-  def up
-    change_column :topics, :level, :decimal
-  end
-
-  def down
-  end
-end
--- db/migrate/20120825181927_drop_tables.rb
+++
@@ -1,9 +0,0 @@
-class DropTables < ActiveRecord::Migration
-  def up
-    drop_table :persons
-    drop_table :companys
-  end
-
-  def down
-  end
-end
